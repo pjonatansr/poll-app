319b8fbf523e1a850087718c3201ba53993bd80c
Pablo Jonatan <this.pablo@gmail.com>
2022-03-01 13:03:08 -0300

    feat: added member

---
+++ poll/src/main/java/com/coop/example/poll/model/Member.java
@@ -0,0 +1,23 @@
+package com.coop.example.poll.model;
+
+public class Member {
+  private long id;
+  private String cpf;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+}
