d081a8b1c75d55f4917bdfa962957afce7ea7c0a
Pablo Jonatan <this.pablo@gmail.com>
2022-03-01 11:50:42 -0300

    feat(poll): added createPoll action

--- poll/src/main/java/com/coop/example/poll/controller/PollController.java
+++ poll/src/main/java/com/coop/example/poll/controller/PollController.java
@@ -8,10 +8,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/polls")
 public class PollController {
 
-  @RequestMapping(value = "api/polls/{id}", method = RequestMethod.GET)
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
   public Poll getPoll(@PathVariable("id") String id) {
+    // TO-DO: get persited poll
     return new Poll("First Poll!");
+  }
+
+  @RequestMapping(method = RequestMethod.POST)
+  public Poll createPoll(Poll pollInstance) {
+    // TO-DO: persist poll
+    return pollInstance;
   }
 }
