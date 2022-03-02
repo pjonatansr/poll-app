7c88fdf93f3dd41fa27ed3e462b1b9a32dfd517b
Pablo Jonatan <this.pablo@gmail.com>
2022-03-02 19:44:33 -0300

    feat: added lombok to poll application class

--- poll/src/main/java/com/coop/core/poll/PollApplication.java
+++ poll/src/main/java/com/coop/core/poll/PollApplication.java
@@ -1,13 +1,21 @@
 package com.coop.core.poll;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PollApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PollApplication.class, args);
+
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
