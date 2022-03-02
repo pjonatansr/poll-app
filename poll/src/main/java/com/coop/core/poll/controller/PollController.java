54750ae5afd6ae436ac0da2715681e7f08d456f9
Pablo Jonatan <this.pablo@gmail.com>
2022-03-02 19:45:21 -0300

    feat: added save poll to persist entity

--- poll/src/main/java/com/coop/core/poll/controller/PollController.java
+++ poll/src/main/java/com/coop/core/poll/controller/PollController.java
@@ -1,8 +1,12 @@
 package com.coop.core.poll.controller;
 
+import com.coop.core.poll.dto.PollDto;
 import com.coop.core.poll.model.Poll;
+import com.coop.core.poll.service.IPollService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/polls")
 public class PollController {
 
+  @Autowired
+  private IPollService pollService;
+
   @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-  public Poll getPoll(@PathVariable("id") int id) {
+  public Poll getPoll(@PathVariable("id") Long id) {
     // TO-DO: get persited poll
     Poll pollInstance = new Poll("Poll " + id);
     pollInstance.setId(id);
@@ -21,8 +28,9 @@ public class PollController {
   }
 
   @RequestMapping(method = RequestMethod.POST)
-  public Poll createPoll(Poll pollInstance) {
-    // TO-DO: persist poll
+  public Poll save(@RequestBody PollDto pollInput) {
+    Poll pollInstance = pollService.savePoll(pollInput);
+
     return pollInstance;
   }
 }
