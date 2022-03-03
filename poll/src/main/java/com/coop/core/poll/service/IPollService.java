3eb70e4f9b8a5b158f726dfd1d418b93a794eaaf
Pablo Jonatan <this.pablo@gmail.com>
2022-03-03 07:57:15 -0300

    chore: changed pollService to use baseService

--- poll/src/main/java/com/coop/core/poll/service/IPollService.java
+++ poll/src/main/java/com/coop/core/poll/service/IPollService.java
@@ -1,20 +1,9 @@
 package com.coop.core.poll.service;
 
-import java.util.List;
-
+import com.coop.core.common.service.IBaseService;
 import com.coop.core.poll.dto.PollDto;
 import com.coop.core.poll.model.Poll;
 
-public interface IPollService {
-  // save operation
-  Poll savePoll(PollDto pollInput);
+public interface IPollService extends IBaseService<Poll, PollDto> {
 
-  // read operation
-  List<Poll> fetchPollList();
-
-  // update operation
-  Poll updatePoll(PollDto poll);
-
-  // delete operation
-  void deletePollById(Long pollId);
 }
--- poll/src/main/java/com/coop/core/poll/service/PollService.java
+++ poll/src/main/java/com/coop/core/poll/service/PollService.java
@@ -1,10 +1,13 @@
 package com.coop.core.poll.service;
 
+import java.util.Date;
 import java.util.List;
 
 import com.coop.core.poll.dto.PollDto;
 import com.coop.core.poll.model.Poll;
+import com.coop.core.poll.model.Session;
 import com.coop.core.poll.repository.IPollRepository;
+import com.coop.core.poll.repository.ISessionRepository;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,36 +21,35 @@ public class PollService implements IPollService {
   private IPollRepository pollRepository;
 
   @Autowired
+  private ISessionRepository sessionRepository;
+
+  @Autowired
   private ModelMapper modelMapper;
 
   @Value("${poll.default.duration}")
   private Integer defaultDuration;
 
   @Override
-  public Poll savePoll(PollDto poll) {
+  public Poll save(PollDto poll) {
     if (poll.getDurationMinutes() == null) {
       poll.setDurationMinutes(defaultDuration);
     }
 
     Poll pollInstance = modelMapper.map(poll, Poll.class);
+    pollInstance = pollRepository.save(pollInstance);
 
-    return pollRepository.save(pollInstance);
+    Date startDate = poll.getStartDate();
+    
+    Session session = new Session(pollInstance, startDate);
+
+    sessionRepository.save(session);
+
+    return pollInstance;
   }
 
   @Override
-  public List<Poll> fetchPollList() {
+  public List<Poll> fetchList() {
     return (List<Poll>) pollRepository.findAll();
   }
 
-  @Override
-  public Poll updatePoll(PollDto poll) {
-    // TO-DO: update poll
-    return null;
-  }
-
-  @Override
-  public void deletePollById(Long PollId) {
-
-    pollRepository.deleteById(PollId);
-  }
 }
