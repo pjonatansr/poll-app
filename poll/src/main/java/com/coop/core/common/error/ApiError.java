6beafacc2162d94da6f8fe804036b37ca920eb8d
Pablo Jonatan <this.pablo@gmail.com>
2022-03-03 14:16:37 -0300

    fix: fixed timestamp pattern

--- poll/src/main/java/com/coop/core/common/error/ApiError.java
+++ poll/src/main/java/com/coop/core/common/error/ApiError.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 public class ApiError {
 
   private HttpStatus status;
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
   private LocalDateTime timestamp;
   private String message;
   private String debugMessage;
