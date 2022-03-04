689fc840e5994b8d8cc15772859ae3aa3e0ca2b2
Pablo Jonatan <this.pablo@gmail.com>
2022-03-04 02:04:13 -0300

    feat(member): added external data consumer

---
+++ poll/src/main/java/com/coop/acl/member/dto/MemberDto.java
@@ -0,0 +1,13 @@
+package com.coop.acl.member.dto;
+
+public class MemberDto {
+  private String status;
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+}
---
+++ poll/src/main/java/com/coop/acl/member/service/MemberConsumerService.java
@@ -0,0 +1,27 @@
+package com.coop.acl.member.service;
+
+import com.coop.acl.member.dto.MemberDto;
+import com.coop.core.poll.service.IMemberConsumerService;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class MemberConsumerService implements IMemberConsumerService {
+
+  @Value("${member.service.default.url}")
+  private String defaultUrl;
+
+  @Value("${member.default.authorized.status}")
+  private String authorizedStatus;
+
+  @Override
+  public boolean isMemberAbleToVote(String cpf) {
+    RestTemplate restTemplate = new RestTemplate();
+    MemberDto memberDto = restTemplate.getForObject(defaultUrl, MemberDto.class);
+    boolean isAuthorized = memberDto.getStatus().equals(authorizedStatus);
+
+    return isAuthorized;
+  }
+}
